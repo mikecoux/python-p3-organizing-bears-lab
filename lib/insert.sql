d2a8fce9db273ed82c329faac9122134e5aeeb4a
Michael Marcoux <michaelmarcoux99@gmail.com>
2023-05-04 14:42:47 -0600

    completed

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 2, 'M', 'brown', 'calm', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 5, 'M', 'white', 'intense', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 3, 'F', 'black', 'chill', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 2, 'M', 'brown', 'rigid', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 5, 'F', 'spotted', 'angry', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 7, 'M', 'green', 'pissed', false);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 1, 'F', 'blue', 'tranquil', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, NULL, NULL, 'brown', 'calm', true);
